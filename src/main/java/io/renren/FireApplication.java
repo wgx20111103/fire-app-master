08e181ae1612aa8a82f0e8f01a9a65d7f3a7bc6e
wgx20111103 <1037729032@qq.com>
2025-03-02 16:33:11 +0800

    提交代码

---
+++ src/main/java/io/renren/FireApplication.java
@@ -0,0 +1,14 @@
+
+package io.renren;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FireApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FireApplication.class, args);
+	}
+
+}
